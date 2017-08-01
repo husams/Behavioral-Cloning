2e9aa99f3db03433a5832e77926dc89b664fb53e
Husam Senussi <husam.senussi@me.com>
2017-08-01 22:27:02 +0100

    Improve model

--- model.py
+++ model.py
@@ -48,7 +48,7 @@ model.add(Conv2D(64, (5,5), padding="same"))
 model.add(Activation("relu"))
 model.add(MaxPool2D(pool_size=(2, 2)))
 model.add(Flatten())
-model,add(Dense(1024))
+model.add(Dense(1024))
 model.add(Dropout(0.5))
 model.add(Dense(1))
 
