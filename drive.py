a333d901034e1313a8b4c5a3df239db591bf6e8f
husams <husam.senussi@me.com>
2017-08-14 17:26:56 +0100

    Add files via upload

--- drive.py
+++ drive.py
@@ -11,6 +11,8 @@ import eventlet.wsgi
 from PIL import Image
 from flask import Flask
 from io import BytesIO
+from data import preprocess
+
 
 from keras.models import load_model
 import h5py
@@ -44,7 +46,7 @@ class SimplePIController:
 
 
 controller = SimplePIController(0.1, 0.002)
-set_speed = 9
+set_speed = 20
 controller.set_desired(set_speed)
 
 
@@ -60,8 +62,9 @@ def telemetry(sid, data):
         # The current image from the center camera of the car
         imgString = data["image"]
         image = Image.open(BytesIO(base64.b64decode(imgString)))
-        image_array = np.asarray(image)
-        steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
+        image_array    = np.asarray(image)
+        image_array64  = preprocess(image_array)
+        steering_angle = float(model.predict(image_array64[None, :, :, :], batch_size=1))
 
         throttle = controller.update(float(speed))
 
